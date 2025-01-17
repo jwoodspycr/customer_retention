d593837c3c695ac684508c6435a3738d9a19affb
jwoodspycr <jwoodspycr>
2025-01-17 16:20:08 +0000

    initial

---
+++ Untitled Notebook 2025-01-17 11_16_35.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import keras
+
+# COMMAND ----------
+
+print (keras.__version__)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
